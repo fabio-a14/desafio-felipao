a7725816d4c35a089e69bafdc30b2aabb170d8e0
Fabio azevedo de Matos <fbo_matos@hotmail.com>
2023-11-28 17:53:40 -0300

    desafio concluido

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 let nome = "Cloud";
-let xp = 9060;
+let xp = 12000;
 
 if (xp < 1000) {
   console.log("O Herói  " + nome, "está no nível  Ferro");
-} else if (xp >= 1001 && xp === 2000) {
+} else if (xp >= 1001 && xp <= 2000) {
   console.log("O Herói  " + nome, "está no nível  Bronze");
-} else if (xp >= 2001 && xp === 5000) {
+} else if (xp >= 2001 && xp < 5000) {
   console.log("O Herói  " + nome, "está no nível  Prata");
-} else if (xp >= 6001 && xp === 7000) {
+} else if (xp >= 6001 && xp <= 7000) {
   console.log("O Herói  " + nome, "está no nível  Ouro");
-} else if (xp >= 7001 && xp === 8000) {
+} else if (xp >= 7001 && xp <= 8000) {
   console.log("O Herói  " + nome, "está no nível  Platina");
-} else if (xp >= 8001 && xp === 9000) {
+} else if (xp >= 8001 && xp <= 9000) {
   console.log("O Herói  " + nome, "está no nível  Ascendente");
-} else if (xp >= 9001 && xp >= 10000) {
+} else if (xp >= 9001 && xp <= 10000) {
   console.log("O Herói  " + nome, "está no nível  Imortal");
 } else if (xp >= 10001) {
   console.log("O Herói de nome " + nome, "está no nível  Radiante");
-}
+} 
